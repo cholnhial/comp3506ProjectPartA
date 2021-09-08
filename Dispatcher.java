fe02547d8738a02334ed29226490f540b823da1d
Chol Nhial <chol.nhial.chol@gmail.com>
2021-09-08 17:00:32 +1000

    Implementation of Dispatcher todate

--- Dispatcher.java
+++ Dispatcher.java
@@ -1,12 +1,17 @@
+import java.util.Arrays;
+
 public class Dispatcher extends DispatcherBase {
+
+    private final PlanePriorityQueue planePriorityQueue = new PlanePriorityQueue();
+
     @Override
     public int size() {
-        return 0;
+        return planePriorityQueue.getSize();
     }
 
     @Override
     public void addPlane(String planeNumber, String time) {
-
+        planePriorityQueue.add(new Plane(planeNumber, time));
     }
 
     @Override
@@ -21,10 +26,139 @@ public class Dispatcher extends DispatcherBase {
 
     @Override
     public boolean isPresent(String planeNumber) {
-        return false;
+        return planePriorityQueue.contains(planeNumber);
     }
 
     /* Implement all the necessary methods of Dispatcher here */
 }
 
 /* Add any additional helper classes here */
+
+class PlanePriorityQueue {
+    private PlaneLinkedListNode head;
+    private int size;
+
+    public PlanePriorityQueue () {
+        size = 0;
+        head = null;
+    }
+
+    public void add(Plane plane) {
+        PlaneLinkedListNode newNode = new PlaneLinkedListNode(plane);
+        newNode.next = null;
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            PlaneLinkedListNode current = head;
+            PlaneLinkedListNode previous = null;
+
+            while(current != null) {
+                previous = current;
+
+                // Found place somewhere in the center
+                if (newNode.plane.compareTo(current.plane) <= 0 && current.next != null &&
+                        newNode.plane.compareTo(current.next.plane) <= 0) {
+                    PlaneLinkedListNode after = current.next;
+                    current.next = newNode;
+                    newNode.next = after;
+                    break;
+                }
+
+                current = current.next;
+            }
+
+            // Not place found, put at the end
+            if (current == null) {
+                previous.next = newNode;
+            }
+        }
+
+        size++;
+    }
+
+    public Plane removePlane(String planeNumber) {
+        PlaneLinkedListNode current = head;
+
+        // If at head
+        if (current.plane.getPlaneNumber().equals(planeNumber)) {
+            head = head.next;
+            size--;
+        }
+
+        // the other cases
+        PlaneLinkedListNode previous = current;
+        Plane planeFound = null;
+        current = current.next;
+        while (current != null) {
+            if (current.plane.getPlaneNumber().equals(planeNumber)) {
+                previous.next = current.next;
+                planeFound = current.getPlane();
+            }
+            previous = current;
+            current = current.next;
+        }
+
+        if (planeFound != null) {
+            size--;
+        }
+        return planeFound;
+    }
+
+    boolean contains(String planeNumber) {
+        PlaneLinkedListNode current = head;
+        while(current != null) {
+            if(current.plane.getPlaneNumber().equals(planeNumber)) {
+                return true;
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Helper method for testing
+     *
+     * @return array of planes sorted
+     */
+    @Override
+    public String toString() {
+        Plane[] result = new Plane[size];
+        PlaneLinkedListNode current = head;
+        int i = 0;
+        while(current != null) {
+            result[i] = current.plane;
+            current = current.next;
+            i++;
+        }
+
+        return Arrays.toString(result);
+    }
+
+
+    static class PlaneLinkedListNode {
+        PlaneLinkedListNode next;
+        Plane plane;
+
+        public PlaneLinkedListNode(Plane plane) {
+            this.plane = plane;
+        }
+
+        public PlaneLinkedListNode getNext() {
+            return next;
+        }
+
+        public void setNext(PlaneLinkedListNode node) {
+            this.next = node;
+        }
+
+        public Plane getPlane() {
+            return plane;
+        }
+    }
+}
